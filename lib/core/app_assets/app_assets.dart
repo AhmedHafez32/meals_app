4879bafba52189ca92c3200f88d9e3e5073eefff
AhmedHafez32 <mohahz38556@gmail.com>
2025-09-15 23:09:22 +0300

    config assets

--- lib/core/app_assets/app_assets.dart
+++ lib/core/app_assets/app_assets.dart
@@ -1,3 +1,9 @@
 class AppAssets{
-
+  static const String homeScreenImage = 'assets/images/homeScreenImage.png';
+  static const String onBoardingImage = 'assets/images/onBoardingImage.png';
+  static const String mealDetailsImage = 'assets/images/mealDetailsImage.png';
+  static const String item1image = 'assets/images/item1.png';
+  static const String item2image = 'assets/images/item2.png';
+  static const String item3image = 'assets/images/item3.png';
+  static const String item4image = 'assets/images/item4.png';
 }
